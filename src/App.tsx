e01783e7ebfc72df6db4a86c5dfdae1ba5653121
Ranel <jrrtuble@gmail.com>
2024-12-27 21:09:45 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import "./App.css";
+import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
+
+function App() {
+  const { users, error, isLoading, setUsers, setError } = useUsers();
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    userService.delete(user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Mosh" };
+    setUsers([newUser, ...users]);
+    userService
+      .create(newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    userService.update(updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  return (
+    <>
+      <div>
+        {error && <p className="text-danger">{error}</p>}
+        {isLoading && <div className="spinner-border"></div>}
+        <button className="btn btn-primary mb-3" onClick={addUser}>
+          Add
+        </button>
+        <ul className="list-group">
+          {users.map((user) => (
+            <li
+              key={user.id}
+              className="list-group-item d-flex justify-content-between"
+            >
+              {user.id}, {user.name}
+              <div>
+                <button
+                  className="btn btn-warning mx-1"
+                  onClick={() => updateUser(user)}
+                >
+                  Update
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteUser(user)}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
